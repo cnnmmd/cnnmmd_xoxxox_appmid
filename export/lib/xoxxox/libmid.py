229b092bebcc53d09b2b114c311951ceb0c5ace7
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-04 02:35:12 +0900

    fix output from memory node

--- export/lib/xoxxox/libmid.py
+++ export/lib/xoxxox/libmid.py
@@ -1,3 +1,5 @@
 class LibMid:
   dicprc = {} # 処理の内容 # LibMid.dicprc["X"] = {"frm": "X.X.X", "arg": [...], "cnf": [...], "mem": [...], "syn": (True|False)}
   plugin = {} # プラグイン
+  values = {} # 内容の格納（ターンごとに内容を消去する）
+  memory = {} # 内容の格納（ターンごとに内容を消去せず）
