16c11ddfc31e108e1c86741b0fa09013dc90d82a
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-15 14:13:43 +0900

    wip

--- manage/bin/params.sh
+++ manage/bin/params.sh
@@ -1,5 +1,5 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envmid
 cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envmid.txt
 cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_appmid.yml
-pthdoc="${pthtop}"/export/cnf/xoxxox/docker
-imgtgt='xoxxox_envmid:001'
+imgtgt='xoxxox/envmid:001'
 nettgt='xoxxox_netmid'
